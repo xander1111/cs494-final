8efeb1508a87f0712a4865647be42931a26469bd
Xander Bailey <xander.bailey@gmail.com>
2025-11-30 17:45:17 -0800

    Add edges page

---
+++ src/app/edges/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { useState } from "react";
+
+import { List, MenuItem, Stack, Typography } from "@mui/material";
+
+
+import StyledTextField from "@/components/styledTextField";
+import StyledSelect from "@/components/styledSelect";
+import { CaseCard } from "@/components/caseCard";
+import { StatsCard } from "@/components/statsCard";
+import { StatLine } from "@/components/statLine";
+
+
+export default function Home() {
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [filter, setFilter] = useState<string>("")
+    const [sortBy, setSortBy] = useState<string>("alphabetical")
+    const [ascending, setAscending] = useState<boolean>(false)
+
+    return (
+        <Stack direction='row' width='80%' height='calc(100vh - 4rem)' justifyContent='space-evenly'>
+            <Stack direction='column' gap={2} height='100%' width='40%' justifyContent='flex-start' pt={5} >
+                <Stack direction='row' pl={1} pr={1} gap={2} width='100%' justifyContent='flex-start'>
+                    <StyledTextField label="Search" type='search' onChange={e => { setSearchTerm(e.target.value) }} />
+
+                    <Stack direction='row' >
+                        <Typography variant='cardSubheader'>Sort By:</Typography>
+                        <StyledSelect
+                            value={sortBy}
+                            onChange={e => { setSortBy(e.target.value as string) }}
+                        >
+                            <MenuItem value="alphabetical">Alphabetical</MenuItem>
+                            <MenuItem value="learned">Learned</MenuItem>
+                        </StyledSelect>
+                    </Stack>
+                    {/* TODO Filter chip */}
+                </Stack>
+                <List
+                    sx={{
+                        maxHeight: '100%',
+                        overflow: 'auto',
+                        width: '100%',
+                        pl: 1,
+                        pr: 1,
+                    }}
+                >
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                    <CaseCard case="AB" algorithm="R2' U: [R2, S]" color='primary' />
+                    <CaseCard case="BS" algorithm="(U M U M')2" color='primary' />
+                </List>
+            </Stack>
+            <StatsCard type="Edge">
+                <StatLine category="Total" numericCompletion="x/y" percentCompletion={60} color='error' />
+                <StatLine category="4 Move" numericCompletion="x/y" percentCompletion={12} color='success' />
+                <StatLine category="5 Move" numericCompletion="x/y" percentCompletion={34} color='success' />
+                <StatLine category="M-Swap" numericCompletion="x/y" percentCompletion={56} color='secondary' />
+                <StatLine category="U-Swap" numericCompletion="x/y" percentCompletion={78} color='secondary' />
+                <StatLine category="E-Swap" numericCompletion="x/y" percentCompletion={90} color='secondary' />
+                <StatLine category="S-Swap" numericCompletion="x/y" percentCompletion={12} color='secondary' />
+                <StatLine category="F-Swap" numericCompletion="x/y" percentCompletion={23} color='secondary' />
+                <StatLine category="D-Swap" numericCompletion="x/y" percentCompletion={34} color='secondary' />
+                <StatLine category="S-Insert" numericCompletion="x/y" percentCompletion={45} color='secondary' />
+                <StatLine category="Algorithm" numericCompletion="x/y" percentCompletion={56} color='primary' />
+            </StatsCard>
+        </Stack>
+    );
+}
