ce98b297e00d11fb9b6e1de4ab965c3706818255
Xander Bailey <xander.bailey@gmail.com>
2025-11-30 17:41:01 -0800

    Add padding to case list

--- src/app/corners/page.tsx
+++ src/app/corners/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
     return (
         <Stack direction='row' width='80%' height='calc(100vh - 4rem)' justifyContent='space-evenly'>
-            <Stack direction='column' gap={2} height='100%' width='40%' justifyContent='flex-start'>
+            <Stack direction='column' gap={2} height='100%' width='40%' justifyContent='flex-start' pt={5} >
                 <Stack direction='row' pl={1} pr={1} gap={2} width='100%' justifyContent='flex-start'>
                     <StyledTextField label="Search" type='search' onChange={e => { setSearchTerm(e.target.value) }} />
 
